b8f92bf9159821f1df9c6b550399d5a519cf61f5
wma9760 <49679469+wma9760@users.noreply.github.com>
2023-08-09 13:06:27 +0500

    frontend homepage

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     function index(){
-        return 'this is homepage';
+        return view('welcome');
     }
 }
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -246,7 +246,7 @@
                     <div class="row align-items-center">
                         <div class="col-lg-3 col-md-3 col-4">
                             <div class="logo">
-                                <a href="{{route('home')}}"><img src="{{asset('assets/frontend/img/logo/logo.png')}}" alt=""></a>
+                                <a href="{{route('user.home')}}"><img src="{{asset('assets/frontend/img/logo/logo.png')}}" alt=""></a>
                             </div>
                         </div>
                         <div class="col-lg-6 colm_none">
@@ -653,34 +653,34 @@
     <!-- JS
 ============================================ -->
     <!--jquery min js-->
-    <script src="{{asset('assets/js/vendor/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/vendor/jquery-3.4.1.min.js')}}"></script>
     <!--popper min js-->
-    <script src="{{asset('assets/js/popper.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/popper.js')}}"></script>
     <!--bootstrap min js-->
-    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/bootstrap.min.js')}}"></script>
     <!--owl carousel min js-->
-    <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/owl.carousel.min.js')}}"></script>
     <!--slick min js-->
-    <script src="{{asset('assets/js/slick.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/slick.min.js')}}"></script>
     <!--magnific popup min js-->
-    <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/jquery.magnific-popup.min.js')}}"></script>
     <!--counterup min js-->
-    <script src="{{asset('assets/js/jquery.counterup.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/jquery.counterup.min.js')}}"></script>
     <!--jquery countdown min js-->
-    <script src="{{asset('assets/js/jquery.countdown.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/jquery.countdown.js')}}"></script>
     <!--jquery ui min js-->
-    <script src="{{asset('assets/js/jquery.ui.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/jquery.ui.js')}}"></script>
     <!--jquery elevatezoom min js-->
-    <script src="{{asset('assets/js/jquery.elevatezoom.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/jquery.elevatezoom.js')}}"></script>
     <!--isotope packaged min js-->
-    <script src="{{asset('assets/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/isotope.pkgd.min.js')}}"></script>
     <!--slinky menu js-->
-    <script src="{{asset('assets/js/slinky.menu.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/slinky.menu.js')}}"></script>
     <!-- Plugins JS -->
-    <script src="{{asset('assets/js/plugins.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins.js')}}"></script>
 
     <!-- Main JS -->
-    <script src="{{asset('assets/js/main.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/main.js')}}"></script>
 
 
 
