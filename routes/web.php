19e5ad80f8f6f7aad464601bf6daae482df2bf7d
Hammad Zafar <hammadarain05@gmail.com>
2023-08-09 12:21:11 +0500

    base stablished

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\User\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +15,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+
+//User Routes
+
+//Home Controller
+Route::controller(HomeController::class)->group(function () {
+
+    Route::get('/','index');
+    Route::get('home','index');
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Admin Routes
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware([])->prefix('admin')->group(function () {
+
+    //Dashboard Controller
+    Route::controller(DashboardController::class)->group(function () {
+
+        Route::get('/','index');
+        Route::get('dashboard','index');
+
+    });
+
 });
+
+
 
 require __DIR__.'/auth.php';
