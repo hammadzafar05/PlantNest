e3967d5de442e6f662787d58149ba03da852df5c
wma9760 <49679469+wma9760@users.noreply.github.com>
2023-08-10 11:54:18 +0500

    shop page products fetched with pagination

--- app/Http/Controllers/User/ShopController.php
+++ app/Http/Controllers/User/ShopController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index(){
-        return view('pages.shop.index');
+    public function index($id=null){
+        $query = Product::query();
+
+        if ($id !== null) {
+            $query->whereHas('category', function ($subquery) use ($id) {
+                $subquery->where('id', $id);
+            });
+        }
+    
+        $products = $query->with('plantInfo', 'category', 'images')
+                          ->paginate(12);
+        return view('pages.shop.index',compact('products'));
     }
     public function detail(){
         return view('pages.shop.detail');
