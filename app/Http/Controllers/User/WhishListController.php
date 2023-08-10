bb3a6bb39a7d0986ea51689527d56719a752b58d
wma9760 <49679469+wma9760@users.noreply.github.com>
2023-08-10 16:54:28 +0500

    cart and wishlist api

--- app/Http/Controllers/User/WhishListController.php
+++ app/Http/Controllers/User/WhishListController.php
@@ -3,12 +3,53 @@
 
 namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
-
+use App\Models\WishlistItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WhishListController extends Controller
 {
-    public function index(){
-        return view('pages.wishlist.index');
+   
+    public function index()
+    {
+        $userId = Auth::user()->id;
+        $wishlist = WishlistItem::where('user_id', $userId)->get();
+        return view('pages.wishlist.index',compact('wishlist'));
+    }
+
+    public function addToWishlist($id)
+    {
+        $userId = Auth::user()->id;
+
+        // Check if the item is already in the wishlist
+        $existingItem = WishlistItem::where('user_id', $userId)
+            ->where('product_id', $id)
+            ->first();
+
+        if (!$existingItem) {
+            $wishlistItem = new WishlistItem();
+            $wishlistItem->user_id = $userId;
+            $wishlistItem->product_id = $id;
+            $wishlistItem->save();
+            return response()->json(['message' => 'Item added to wishlist', 'wishlistItem' => $wishlistItem]);
+        }
+
+        return response()->json(['message' => 'Item is already in the wishlist']);
+    }
+
+    public function removeFromWishlist($id)
+    {
+        $userId = Auth::user()->id;
+
+        $wishlistItem = WishlistItem::where('user_id', $userId)
+            ->where('product_id', $id)
+            ->first();
+
+        if ($wishlistItem) {
+            $wishlistItem->delete();
+            return response()->json(['message' => 'Item removed from wishlist']);
+        }
+
+        return response()->json(['message' => 'Item not found in wishlist'], 404);
     }
 }
