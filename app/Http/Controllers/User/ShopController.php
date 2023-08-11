e3e70e9107c61b199cbfc94f5c767361ad397ef3
wma9760 <49679469+wma9760@users.noreply.github.com>
2023-08-11 17:39:37 +0500

    controllers updated

--- app/Http/Controllers/User/ShopController.php
+++ app/Http/Controllers/User/ShopController.php
@@ -10,16 +10,46 @@ class ShopController extends Controller
 {
     public function index($id = null)
     {
+        // $query = Product::query();
+
+        // if ($id !== null) {
+        //     $query->whereHas('category', function ($subquery) use ($id) {
+        //         $subquery->where('id', $id);
+        //     });
+        // }
+
+        // $products = $query->with('plantInfo', 'category', 'images')
+        //     ->paginate(12);
         $query = Product::query();
+        // Check if products are empty
+        
 
         if ($id !== null) {
+        $query = Product::query();
+        $check = $query->where('category_id',$id)->get(); // Fetch products from the provided category
+        if ($check->isEmpty()) {
+            $products = Product::whereHas('category', function ($query) use ($id) {
+                $query->where('parent_id', $id);
+            })
+            ->with('images', 'plantInfo')
+            ->paginate(12);
+        }
+        else if ($id !== null) {
             $query->whereHas('category', function ($subquery) use ($id) {
                 $subquery->where('id', $id);
             });
+            $products = $query->with('plantInfo', 'images')
+            ->paginate(12);
         }
 
-        $products = $query->with('plantInfo', 'category', 'images')
+        else{
+            $products = $query->with('plantInfo', 'images')
             ->paginate(12);
+            
+        }
+        // $products = $query->with('plantInfo', 'category', 'images');
+    }
+
         return view('pages.shop.index', compact('products'));
     }
     public function detail($id)
