dc8eb7d213235e36f739907148162c6d3d59e528
Junaid0987 <113843901+Junaid0987@users.noreply.github.com>
2023-08-12 14:07:35 +0500

    Update ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -217,18 +217,18 @@ class ProductController extends Controller
 
     public function productDetail($id)
     {
-    //   $productArray=Product::with([
-    //     'category'=>function($query){
-    //         $query->select('id','name','parent_id');
-    //     },
-    //     'images'=>function($query){
-    //         $query->select('id','image_url','product_id');
-    //     }])
-    //     ->leftjoin('reviews','products.id','=','reviews.product_id')
-    //     ->leftjoin('users','users.id','=','reviews.user_id')
-    //     ->where('products.id','=',$id)
-    //   ->get()->first()->toArray();
-    //   dd($productArray);
+      $productArray=Product::with([
+        'category'=>function($query){
+            $query->select('id','name','parent_id');
+        },
+        'images'=>function($query){
+            $query->select('id','image_url','product_id');
+        }])
+        ->leftjoin('reviews','products.id','=','reviews.product_id')
+        ->leftjoin('users','users.id','=','reviews.user_id')
+        ->where('products.id','=',$id)
+      ->get()->first()->toArray();
+      dd($productArray);
       return view('admin.pages.product.detail',['productArray'=>$productArray]);
     }
 
